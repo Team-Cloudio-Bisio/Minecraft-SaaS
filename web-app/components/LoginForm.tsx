c8a34843abc9d171d2fdaada8c52cfc9c6ac438f
Federico Andrucci <federico.andrucci@gmail.com>
2023-07-05 17:52:53 +0200

    s

--- web-app/components/LoginForm.tsx
+++ web-app/components/LoginForm.tsx
@@ -23,7 +23,7 @@ export default function LoginForm() {
       const post = async () => {
         const u: User = {username: email, userPassword: password};
         // const res = await fetch(`/api/login?api=${process.env.NEXT_PUBLIC_API_KEY}`, { method: "POST", body: JSON.stringify(u)});
-        const res = await fetch("http://51.138.90.181:81/Account/login", { method: "POST", body: JSON.stringify(u)});
+        const res = await fetch("https://51.138.90.181:81/Account/login", { method: "POST", body: JSON.stringify(u)});
         return res.json();
       }
 
